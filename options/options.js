7ef92671ecbff24a7f27519d72190c17e8c61094
Crowdbotics <team@crowdbotics.com>
2023-06-30 18:55:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-sound-41830",
+  url: "https://floral-sound-41830.botics.co",
+  api: "https://floral-sound-41830.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
